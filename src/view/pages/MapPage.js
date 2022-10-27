15723e0af937e4ae359d143ea6cc32e85d27aa46
nuuuwan <nuuuwan@gmail.com>
2022-10-27 13:34:05 +0530

    feat: Use SliderSubRegionType

--- src/view/pages/MapPage.js
+++ src/view/pages/MapPage.js
@@ -7,14 +7,15 @@ import Partition from "../../nonview/core/Partition";
 import AppColors from "../../view/_constants/AppColors";
 import SliderMaxSeatsPerGroup from "../../view/atoms/SliderMaxSeatsPerGroup";
 import SliderSeats from "../../view/atoms/SliderSeats";
+import SliderSubRegionType from "../../view/atoms/SliderSubRegionType";
 import GeoJSONView from "../../view/molecules/GeoJSONView";
 import PartitionView from "../../view/molecules/PartitionView";
 import AbstractInnerPage from "../../view/pages/AbstractInnerPage";
 
 const DEFAULT_N_SEATS = 19;
 const DEFAULT_MAX_SEATS_PER_GROUP = 19;
-const DEFAULT_SUBREGION_TYPE = 'dsd';
-const DEFAULT_REGION_ID = 'LK-11';
+const DEFAULT_SUBREGION_TYPE = "dsd";
+const DEFAULT_REGION_ID = "LK-11";
 export default class MapPage extends AbstractInnerPage {
   constructor(props) {
     super(props);
@@ -24,7 +25,8 @@ export default class MapPage extends AbstractInnerPage {
       idToGroup: null,
       nSeats: DEFAULT_N_SEATS,
       maxSeatsPerGroup: DEFAULT_MAX_SEATS_PER_GROUP,
-      regionID: DEFAULT_REGION_ID, subRegionType: DEFAULT_SUBREGION_TYPE,
+      regionID: DEFAULT_REGION_ID,
+      subRegionType: DEFAULT_SUBREGION_TYPE,
     };
   }
   get page() {
@@ -62,6 +64,17 @@ export default class MapPage extends AbstractInnerPage {
     this.setState({ geoJSON, partition, idToGroup, maxSeatsPerGroup });
   }
 
+  async setSubRegionType(subRegionType) {
+    const { regionID, nSeats, maxSeatsPerGroup } = this.state;
+    const geoJSON = await new GeoJSON(regionID, subRegionType).read();
+    const { partition, idToGroup } = await this.loadStateGeo(
+      nSeats,
+      maxSeatsPerGroup,
+      geoJSON
+    );
+    this.setState({ subRegionType, geoJSON, partition, idToGroup });
+  }
+
   async loadStateGeo(nSeats, maxSeatsPerGroup, geoJSON) {
     const partition = Partition.fromGeoJSONFeatures(geoJSON.features, nSeats);
     partition.partitionAll(maxSeatsPerGroup);
@@ -69,7 +82,7 @@ export default class MapPage extends AbstractInnerPage {
     return { partition, idToGroup };
   }
   async componentDidMount() {
-    const { nSeats, maxSeatsPerGroup,regionID, subRegionType } = this.state;
+    const { nSeats, maxSeatsPerGroup, regionID, subRegionType } = this.state;
     const geoJSON = await new GeoJSON(regionID, subRegionType).read();
     const { partition, idToGroup } = await this.loadStateGeo(
       nSeats,
@@ -80,13 +93,23 @@ export default class MapPage extends AbstractInnerPage {
   }
 
   render() {
-    const { nSeats, maxSeatsPerGroup, geoJSON, partition, idToGroup } =
-      this.state;
+    const {
+      nSeats,
+      maxSeatsPerGroup,
+      subRegionType,
+      geoJSON,
+      partition,
+      idToGroup,
+    } = this.state;
     if (!geoJSON) {
       return "Loading...";
     }
     return (
       <Box>
+        <SliderSubRegionType
+          subRegionType={subRegionType}
+          setSubRegionType={this.setSubRegionType.bind(this)}
+        />
         <SliderSeats nSeats={nSeats} setNSeats={this.setNSeats.bind(this)} />
         <SliderMaxSeatsPerGroup
           maxSeatsPerGroup={maxSeatsPerGroup}
@@ -94,7 +117,6 @@ export default class MapPage extends AbstractInnerPage {
         />
         <GeoJSONView geoJSON={geoJSON} idToGroup={idToGroup} />
         <PartitionView partition={partition} nSeats={nSeats} />
-
       </Box>
     );
   }
