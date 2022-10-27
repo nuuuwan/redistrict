21e2f567dc9e1523b5ca1ea5472e3601f8f6c512
nuuuwan <nuuuwan@gmail.com>
2022-10-27 09:09:25 +0530

    feat: + PartitionRegionIdx

---
+++ src/nonview/core/PartitionRegionIdx.js
@@ -0,0 +1,29 @@
+import MathX from "../../nonview/base/MathX";
+import PartitionRegion from "../../nonview/core/PartitionRegion";
+
+export default class PartitionRegionIdx {
+  static fromGeoJSONFeatures(geojsonFeatures) {
+    const idx = geojsonFeatures.reduce(function (idx, geojsonFeature) {
+      const partitionRegion =
+        PartitionRegion.fromGeoJSONFeature(geojsonFeature);
+      idx[partitionRegion.id] = partitionRegion;
+      return idx;
+    }, {});
+    return new PartitionRegionIdx(idx);
+  }
+
+  constructor(idx) {
+    this.idx = idx;
+  }
+
+  get partitionRegionList() {
+    return Object.values(this.idx);
+  }
+
+  get totalPop() {
+    return MathX.sumGeneric(
+      this.partitionRegionList,
+      (partitionRegion) => partitionRegion.pop
+    );
+  }
+}
