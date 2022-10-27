8078bc17635328365430f78073f16db709d6874b
nuuuwan <nuuuwan@gmail.com>
2022-10-27 10:26:27 +0530

    feat: + getForKey

--- src/nonview/base/Color.js
+++ src/nonview/base/Color.js
@@ -1,10 +1,27 @@
 function getRandomInt(min, max) {
   return parseInt(min + Math.random() * (max - min));
 }
+
+const KEY_COLORS_COLOR0 = getRandomInt(0, 360);
+let n = 0;
+let keyToN = {};
+const DEFAULT_SLA = [100, 50, 1];
+
 export default class Color {
+  static getForKey(key) {
+    if (!keyToN[key]) {
+      keyToN[key] = n;
+      n += 1;
+    }
+    const incr = parseInt(360 / n);
+    const h = (KEY_COLORS_COLOR0 + keyToN[key] * incr) % 360;
+    const [s, l, a] = DEFAULT_SLA;
+    return Color.hsla(h, s, l, a);
+  }
+
   static getRandom() {
     const h = getRandomInt(0, 360);
-    const [s, l, a] = [100, 50, 1];
+    const [s, l, a] = DEFAULT_SLA;
     return Color.hsla(h, s, l, a);
   }
 
