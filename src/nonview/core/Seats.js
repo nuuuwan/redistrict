b273f812131c39b9aa2cac1af02c22782690d8cf
nuuuwan <nuuuwan@gmail.com>
2022-10-27 12:52:02 +0530

    feat: + Seats

---
+++ src/nonview/core/Seats.js
@@ -0,0 +1,34 @@
+import MathX from "../../nonview/base/MathX";
+export default class Seats {
+  static divideSeats(nSeats, partition) {
+
+    let groupToPop = {};
+    let totalPop = 0;
+     /* eslint-disable no-unused-vars */
+    for (let [group, {idList, nSeats}] of Object.entries(partition.groupToIDListAndNSeats)) {
+      const pop = partition.partitionRegionIdx.getTotalPop(idList);
+      totalPop += pop;
+      groupToPop[group] = pop;
+    }
+
+    let groupToSeats = {};
+    let remSeats = nSeats;
+    let groupAndRem = [];
+    for (let [group, pop] of Object.entries(groupToPop)) {
+      const seatsFloat = nSeats * pop / totalPop;
+      const seatsInt = parseInt(seatsFloat);
+      groupToSeats[group] = seatsInt;
+      const rem = seatsFloat - seatsInt;
+      remSeats -= seatsInt;
+      groupAndRem.push([group, rem]);
+    }
+
+    const sortedGroupAndRem = groupAndRem.sort((a, b) => b[1] - a[1]);
+    for (let i of MathX.range(0, remSeats)) {
+      const group = sortedGroupAndRem[i][0];
+      groupToSeats[group] += 1;
+    }
+    return groupToSeats;
+
+  }
+}
