3a359d5384f44886eaf8f9a547f39df4c1f3edd2
nuuuwan <nuuuwan@gmail.com>
2022-10-27 08:17:13 +0530

    feat: +getGroupColor

--- src/view/molecules/GeoJSONView.js
+++ src/view/molecules/GeoJSONView.js
@@ -1,14 +1,20 @@
 import BBox from "../../nonview/base/geo/BBox";
 
-
 import GeoJSONFeatureView from "../../view/molecules/GeoJSONFeatureView";
-import Partition from "../../nonview/core/Partition"
+import Partition from "../../nonview/core/Partition";
 
 const PADDING = 10;
 
 const [MARGIN_WIDTH, MARGIN_HEIGHT] = [100, 100];
-
-
+function getGroupColor(group) {
+  switch(group) {
+    case 0: return 'green';
+    case 1: return 'orange';
+    case 2: return 'yellow';
+    case 3: return 'maroon';
+    default: return 'gray';
+  }
+}
 
 export default function GeoJSONView({ geoJSON }) {
   const bbox = BBox.fromGeoJSON(geoJSON);
@@ -17,15 +23,17 @@ export default function GeoJSONView({ geoJSON }) {
     window.innerHeight - MARGIN_HEIGHT,
   ];
   const funcTransform = bbox.getTransform(width, height, PADDING);
-  const featureToColor = Partition.getFeatureToColor(geoJSON.features);
+  const featureToGroup = Partition.getFeatureToGroup(geoJSON.features);
 
   const inner = geoJSON.features.map(function (feature, iFeature) {
+    const group = featureToGroup[feature.id];
+    const color = getGroupColor(group);
     return (
       <GeoJSONFeatureView
         key={"feature-" + feature.id}
         funcTransform={funcTransform}
         feature={feature}
-        color={featureToColor[feature.id]}
+        color={color}
       />
     );
   });
