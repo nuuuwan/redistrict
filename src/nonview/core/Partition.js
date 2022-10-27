fa8b8f3de6611f3b4af4b5e1382869b54e559eb0
nuuuwan <nuuuwan@gmail.com>
2022-10-27 08:25:52 +0530

    refactor: + getTotalPopulation

--- src/nonview/core/Partition.js
+++ src/nonview/core/Partition.js
@@ -3,13 +3,17 @@ import MathX from "../../nonview/base/MathX";
 const N_GROUPS= 6;
 
 export default class Partition {
-  static getFeatureToGroup(geojsonFeatures) {
-    let featureToGroup = {};
-
-    const totalPopulation = MathX.sumGeneric(
+  static getTotalPopulation(geojsonFeatures) {
+    return MathX.sumGeneric(
       geojsonFeatures,
       (feature) => feature.properties.population
     );
+  }
+
+  static getFeatureToGroup(geojsonFeatures) {
+    let featureToGroup = {};
+
+    const totalPopulation = Partition.getTotalPopulation(geojsonFeatures);
 
     const sortedFeatures = geojsonFeatures.sort(function (a, b) {
       return a.properties.centroid[1] > b.properties.centroid[1];
