570577acd14365007ea689827892db10cbf045b4
nuuuwan <nuuuwan@gmail.com>
2022-10-27 12:10:34 +0530

    view: + SliderMaxSeatsPerGroup

---
+++ src/view/atoms/SliderMaxSeatsPerGroup.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
+import Typography from "@mui/material/Typography";
+
+import MathX from "../../nonview/base/MathX";
+
+const [MIN, MAX] = [1, 5];
+
+export default function SliderMaxSeatsPerGroup({
+  maxSeatsPerGroup,
+  setMaxSeatsPerGroup,
+}) {
+  const onChange = function (_, maxSeatsPerGroup) {
+    setMaxSeatsPerGroup(maxSeatsPerGroup);
+  };
+  const marks = MathX.range(MIN, MAX + 1).map(function (value) {
+    return { value, label: value };
+  });
+  return (
+    <Box width={300}>
+      <Typography variant="caption">Max. Seats per Group</Typography>
+      <Slider
+        min={MIN}
+        max={MAX}
+        value={maxSeatsPerGroup}
+        marks={marks}
+        onChange={onChange}
+        aria-label="Small"
+        size="small"
+        valueLabelDisplay="on"
+      />
+    </Box>
+  );
+}
