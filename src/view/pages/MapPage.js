8fd1ef33de48d1206aab076245158cd0d42a6146
nuuuwan <nuuuwan@gmail.com>
2022-10-27 12:10:54 +0530

    feat: + maxSeatsPerGroup

--- src/view/pages/MapPage.js
+++ src/view/pages/MapPage.js
@@ -5,12 +5,14 @@ import GeoJSON from "../../nonview/base/geo/GeoJSON";
 import Partition from "../../nonview/core/Partition";
 
 import AppColors from "../../view/_constants/AppColors";
+import SliderMaxSeatsPerGroup from "../../view/atoms/SliderMaxSeatsPerGroup";
 import SliderSeats from "../../view/atoms/SliderSeats";
 import GeoJSONView from "../../view/molecules/GeoJSONView";
 import PartitionView from "../../view/molecules/PartitionView";
 import AbstractInnerPage from "../../view/pages/AbstractInnerPage";
 
 const DEFAULT_N_SEATS = 3;
+const DEFAULT_MAX_SEATS_PER_GROUP = 1;
 export default class MapPage extends AbstractInnerPage {
   constructor(props) {
     super(props);
@@ -19,6 +21,7 @@ export default class MapPage extends AbstractInnerPage {
       partition: null,
       idToGroup: null,
       nSeats: DEFAULT_N_SEATS,
+      maxSeatsPerGroup: DEFAULT_MAX_SEATS_PER_GROUP,
     };
   }
   get page() {
@@ -37,26 +40,45 @@ export default class MapPage extends AbstractInnerPage {
   }
 
   async setNSeats(nSeats) {
-    const geoJSON = this.state.geoJSON;
-    const { partition, idToGroup } = await this.loadStateGeo(nSeats, geoJSON);
+    const { maxSeatsPerGroup, geoJSON } = this.state;
+    const { partition, idToGroup } = await this.loadStateGeo(
+      nSeats,
+      maxSeatsPerGroup,
+      geoJSON
+    );
     this.setState({ geoJSON, partition, idToGroup, nSeats });
   }
 
-  async loadStateGeo(nSeats, geoJSON) {
+  async setMaxSeatsPerGroup(maxSeatsPerGroup) {
+    const { nSeats, geoJSON } = this.state;
+    const { partition, idToGroup } = await this.loadStateGeo(
+      nSeats,
+      maxSeatsPerGroup,
+      geoJSON
+    );
+    this.setState({ geoJSON, partition, idToGroup, maxSeatsPerGroup });
+  }
+
+  async loadStateGeo(nSeats, maxSeatsPerGroup, geoJSON) {
     const partition = Partition.fromGeoJSONFeatures(geoJSON.features, nSeats);
-    partition.partitionAll();
+    partition.partitionAll(maxSeatsPerGroup);
     const idToGroup = partition.idToGroup;
     return { partition, idToGroup };
   }
   async componentDidMount() {
-    const { nSeats } = this.state;
+    const { nSeats, maxSeatsPerGroup } = this.state;
     const geoJSON = await new GeoJSON("LK-11", "dsd").read();
-    const { partition, idToGroup } = await this.loadStateGeo(nSeats, geoJSON);
+    const { partition, idToGroup } = await this.loadStateGeo(
+      nSeats,
+      maxSeatsPerGroup,
+      geoJSON
+    );
     this.setState({ geoJSON, partition, idToGroup });
   }
 
   render() {
-    const { geoJSON, partition, idToGroup, nSeats } = this.state;
+    const { nSeats, maxSeatsPerGroup, geoJSON, partition, idToGroup } =
+      this.state;
     if (!geoJSON) {
       return "Loading...";
     }
@@ -64,6 +86,10 @@ export default class MapPage extends AbstractInnerPage {
       <Box>
         <GeoJSONView geoJSON={geoJSON} idToGroup={idToGroup} />
         <SliderSeats nSeats={nSeats} setNSeats={this.setNSeats.bind(this)} />
+        <SliderMaxSeatsPerGroup
+          maxSeatsPerGroup={maxSeatsPerGroup}
+          setMaxSeatsPerGroup={this.setMaxSeatsPerGroup.bind(this)}
+        />
         <PartitionView partition={partition} />
       </Box>
     );
