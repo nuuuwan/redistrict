27b81238feec96cd161cac40ee425d51b69a6a90
nuuuwan <nuuuwan@gmail.com>
2022-10-27 13:33:48 +0530

    feat: + SliderSubRegionType

---
+++ src/view/atoms/SliderSubRegionType.js
@@ -0,0 +1,51 @@
+import * as React from "react";
+
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
+import Typography from "@mui/material/Typography";
+
+import MathX from "../../nonview/base/MathX";
+
+const SUB_REGION_TYPES = ["dsd", "gnd"];
+
+export default function SliderSubRegionType({
+  subRegionType,
+  setSubRegionType,
+}) {
+  const selectedValue = SUB_REGION_TYPES.reduce(function (
+    selectedValue,
+    subRegionType2,
+    value
+  ) {
+    if (subRegionType === subRegionType2) {
+      return value;
+    }
+    return selectedValue;
+  },
+  undefined);
+
+  const marks = SUB_REGION_TYPES.map(function (subRegionType, value) {
+    return { value, subRegionType, label: subRegionType.toUpperCase() };
+  });
+
+  const onChange = function (_, newValue) {
+    const subRegionType = marks[newValue].subRegionType;
+    console.debug(subRegionType);
+    setSubRegionType(subRegionType);
+  };
+
+  return (
+    <Box width={200}>
+      <Typography variant="caption">Sub-Region Type</Typography>
+      <Slider
+        value={selectedValue}
+        min={0}
+        max={marks.length - 1}
+        marks={marks}
+        onChange={onChange}
+        aria-label="Small"
+        size="small"
+      />
+    </Box>
+  );
+}
