7a417ab7cd4afd35d8b7267d143f047c8d66fbe6
nuuuwan <nuuuwan@gmail.com>
2022-10-27 11:32:12 +0530

    feat: use SliderSeats

--- src/view/pages/MapPage.js
+++ src/view/pages/MapPage.js
@@ -5,6 +5,7 @@ import GeoJSON from "../../nonview/base/geo/GeoJSON";
 import Partition from "../../nonview/core/Partition";
 
 import AppColors from "../../view/_constants/AppColors";
+import SliderSeats from "../../view/atoms/SliderSeats";
 import GeoJSONView from "../../view/molecules/GeoJSONView";
 import PartitionView from "../../view/molecules/PartitionView";
 import AbstractInnerPage from "../../view/pages/AbstractInnerPage";
@@ -12,7 +13,7 @@ import AbstractInnerPage from "../../view/pages/AbstractInnerPage";
 export default class MapPage extends AbstractInnerPage {
   constructor(props) {
     super(props);
-    this.state = { geoJSON: null };
+    this.state = { geoJSON: null, partition: null, idToGroup: null, nSeats: 1 };
   }
   get page() {
     return "MapPage";
@@ -29,24 +30,33 @@ export default class MapPage extends AbstractInnerPage {
     return AppColors.Primary;
   }
 
-  async componentDidMount() {
-    const geoJSON = await new GeoJSON("LK-11", "dsd").read();
+  async setNSeats(nSeats) {
+    const { geoJSON, partition, idToGroup } = await this.loadStateGeo(nSeats);
+    this.setState({ geoJSON, partition, idToGroup, nSeats });
+  }
 
-    const partition = Partition.fromGeoJSONFeatures(geoJSON.features, 5);
+  async loadStateGeo(nSeats) {
+    const geoJSON = await new GeoJSON("LK-11", "dsd").read();
+    const partition = Partition.fromGeoJSONFeatures(geoJSON.features, nSeats);
     partition.partitionAll();
     const idToGroup = partition.idToGroup;
-
+    return { geoJSON, partition, idToGroup };
+  }
+  async componentDidMount() {
+    const { nSeats } = this.state;
+    const { geoJSON, partition, idToGroup } = await this.loadStateGeo(nSeats);
     this.setState({ geoJSON, partition, idToGroup });
   }
 
   render() {
-    const { geoJSON, partition, idToGroup } = this.state;
+    const { geoJSON, partition, idToGroup, nSeats } = this.state;
     if (!geoJSON) {
       return "Loading...";
     }
     return (
       <Box>
         <GeoJSONView geoJSON={geoJSON} idToGroup={idToGroup} />
+        <SliderSeats nSeats={nSeats} setNSeats={this.setNSeats.bind(this)} />
         <PartitionView partition={partition} />
       </Box>
     );
