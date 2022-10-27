327e6d24b2512b9f5c1ca4033944e10881aec664
nuuuwan <nuuuwan@gmail.com>
2022-10-27 14:55:45 +0530

    feat: Removed label

--- src/view/molecules/GeoJSONFeatureView.js
+++ src/view/molecules/GeoJSONFeatureView.js
@@ -1,11 +1,6 @@
-import LngLat from "../../nonview/base/geo/LngLat";
-import BBox from "../../nonview/base/geo/BBox";
 import GeoJSONPolygonView from "../../view/molecules/GeoJSONPolygonView";
 
-export default function GeoJSONFeatureView({ funcTransform, feature, color, group }) {
-  const [lat, lng] = BBox.getCentroid(LngLat.fromPolygonListList(feature.geometry.coordinates));
-  const [x, y] = funcTransform([lng, lat]);
-
+export default function GeoJSONFeatureView({ funcTransform, feature, color }) {
   return feature.geometry.coordinates.map(function (polygonList, iPolygonList) {
     return polygonList.map(function (polygon, iPolygon) {
       const key = "polygon-" + iPolygonList + "-" + iPolygon;
@@ -16,16 +11,6 @@ export default function GeoJSONFeatureView({ funcTransform, feature, color, grou
             polygon={polygon}
             color={color}
           />
-          <text
-            x={x}
-            y={y}
-            fill="black"
-            stroke="none"
-            fontFamily="sans-serif"
-            fontSize={10}
-          >
-            {group}
-          </text>
         </g>
       );
     });
