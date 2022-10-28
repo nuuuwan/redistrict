271f9fb35f402ec566297cba0cd90c6bfc402ff6
nuuuwan <nuuuwan@gmail.com>
2022-10-28 21:21:51 +0530

    refactor: sort by name; order variables

--- src/view/molecules/PartitionView.js
+++ src/view/molecules/PartitionView.js
@@ -3,13 +3,18 @@ import Seats from "../../nonview/core/Seats";
 import PartitionViewTable from "../../view/molecules/PartitionViewTable";
 
 export default function PartitionView({ nSeats, partition }) {
-  const regionEntIdx = partition.regionEntIdx;
-  const groupToSeats = Seats.divideSeats(nSeats, partition);
-  const totalPop = regionEntIdx.getTotalPop(regionEntIdx.idList);
   const groupToIDList = partition.getGroupToIDList();
-  const sortedGroups = Object.keys(groupToIDList).sort();
-  const nGroups = sortedGroups.length;
   const groupToName = partition.getGroupToName();
+  const groupToSeats = Seats.divideSeats(nSeats, partition);
+
+  const regionEntIdx = partition.regionEntIdx;
+  const totalPop = regionEntIdx.getTotalPop(regionEntIdx.idList);
+
+  const sortedGroups = Object.keys(groupToIDList).sort(
+    (a, b) => (groupToName[a].localeCompare(groupToName[b]))
+  );
+  const nGroups = sortedGroups.length;
+
   const rows = sortedGroups.map(function (group, iGroup) {
     const idList = groupToIDList[group];
     return {
