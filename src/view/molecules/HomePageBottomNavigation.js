3167bea258ed3fdba90a4930f9a682c898b741ef
nuuuwan <nuuuwan@gmail.com>
2022-10-28 10:38:13 +0530

    chore: lint

--- src/view/molecules/HomePageBottomNavigation.js
+++ src/view/molecules/HomePageBottomNavigation.js
@@ -3,7 +3,5 @@ import React from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 
 export default function HomePageBottomNavigation() {
-  return (
-    <BottomNavigation />
-  );
+  return <BottomNavigation />;
 }
