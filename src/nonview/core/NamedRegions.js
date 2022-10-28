c4434344f9554c2dc9f123bb506ed27f3d9a70fb
nuuuwan <nuuuwan@gmail.com>
2022-10-28 12:30:35 +0530

    feat: Infer toplevel name

--- src/nonview/core/NamedRegions.js
+++ src/nonview/core/NamedRegions.js
@@ -1,15 +1,25 @@
-import EntTypes from "../../nonview/base/EntTypes";
+import EntTypes, {ENT_TYPES} from "../../nonview/base/EntTypes";
 import CommonStore from "../../nonview/core/CommonStore";
 
 export default class NamedRegions {
   static infer(regionIDList) {
-    const regionType = EntTypes.getEntType(regionIDList[0]);
     const commonStore = CommonStore.getSingleton();
-    const nameAndPop = regionIDList
-      .map((regionID) => commonStore.allEntIndex[regionType][regionID])
-      .sort(function (a, b) {
-        return b.population - a.population;
-      });
-    return nameAndPop[0].name;
+    const subRegionType = EntTypes.getEntType(regionIDList[0]);
+    const subRegionIndex = commonStore.allEntIndex[subRegionType];
+    const regionIndex = commonStore.allEntIndex[ENT_TYPES.DSD]
+
+    const dsdIDToPop = {};
+    for (let regionID of regionIDList) {
+      const regionEnt = subRegionIndex[regionID];
+      const dsdID = regionEnt.id.substring(0, 7);
+      if (!dsdIDToPop[dsdID]) {
+        dsdIDToPop[dsdID] = 0;
+      }
+      dsdIDToPop[dsdID] += parseInt(regionEnt.population);
+    }
+
+    const mostPopDSDID = Object.entries(dsdIDToPop).sort((a, b) => b[1] - a[1])[0][0];
+    return regionIndex[mostPopDSDID].name;
+
   }
 }
