b936d0ea6b6a96e1b09f69260a621a4ea92b7e0b
nuuuwan <nuuuwan@gmail.com>
2022-10-28 11:38:21 +0530

    nonview: Copied Cache and WWW (new) from sri_lanka

--- src/nonview/base/Cache.js
+++ src/nonview/base/Cache.js
@@ -1,46 +1,16 @@
-const N_MAX_RETRY = 5;
 export default class Cache {
-  constructor(cacheKey) {
-    this.cacheKey = cacheKey;
-  }
-
-  clear() {
-    localStorage.setItem(this.cacheKey, "");
-  }
-
-  set(item) {
-    for (let i = 0; i < N_MAX_RETRY; i++) {
-      try {
-        localStorage.setItem(this.cacheKey, JSON.stringify(item));
-        return true;
-      } catch {
-        localStorage.clear();
-      }
+  static async get(cacheKey, asyncFallback) {
+    const hotItem = localStorage.getItem(cacheKey);
+    if (hotItem) {
+      return JSON.parse(hotItem);
     }
-    return false;
-  }
 
-  getHot() {
-    const hotJSON = localStorage.getItem(this.cacheKey);
-    if (Cache.isValidHotItem(hotJSON)) {
-      return JSON.parse(hotJSON);
-    }
-    throw Error("Item not in cache: " + this.cacheKey);
-  }
-
-  async get(asyncFallback) {
+    const coldItem = await asyncFallback();
     try {
-      return this.getHot();
-    } catch (e) {}
-
-    const cold = await asyncFallback();
-    this.set(cold);
-    return cold;
-  }
-
-  static isValidHotItem(itemJSON) {
-    return (
-      itemJSON && itemJSON !== "" && itemJSON !== null && itemJSON !== "null"
-    );
+      localStorage.setItem(cacheKey, JSON.stringify(coldItem));
+    } catch (QuotaExceededError) {
+      localStorage.clear();
+    }
+    return coldItem;
   }
 }
--- src/nonview/base/WWW.js
+++ src/nonview/base/WWW.js
@@ -1,4 +1,6 @@
-import Cache from "../../nonview/base/Cache";
+import Cache from "./Cache.js";
+
+const CACHE_VERSION = "v20210823";
 
 const JSON_HEADERS = {
   headers: {
@@ -6,31 +8,44 @@ const JSON_HEADERS = {
   },
 };
 
-export default class WWW {
-  constructor(url) {
-    this.url = url;
-  }
+const TSV_HEADERS = {
+  headers: {
+    Accept: "text/csv",
+  },
+};
 
-  static open(url) {
-    window.history.pushState("", "", url);
-    window.location.reload(true);
-  }
+async function jsonNonCache(url) {
+  const response = await fetch(url, JSON_HEADERS);
+  const dataJson = await response.json();
+  return dataJson;
 }
 
-export class JSONWWW extends WWW {
-  async readNoCache() {
-    try {
-      const response = await fetch(this.url, JSON_HEADERS);
-      const data = await response.json();
-      return data;
-    } catch (err) {
-      return null;
-    }
+export default class WWW {
+  static pathJoin(pathFragmentList) {
+    return pathFragmentList.join("/");
   }
 
-  async read() {
-    const cacheKey = "cache-" + this.url;
-    const cache = new Cache(cacheKey);
-    return await cache.get(this.readNoCache.bind(this));
+  static async json(url) {
+    return Cache.get(`WWW.json.${CACHE_VERSION}.${url}`, async function () {
+      return jsonNonCache(url);
+    });
+  }
+
+  static async tsv(url) {
+    const response = await fetch(url, TSV_HEADERS);
+    const content = await response.text();
+    const lines = content.split("\n");
+    const keys = lines[0].split("\t").map((key) => key.replace("\r", ""));
+    const dataList = lines
+      .slice(1, -1)
+      .map(function (line) {
+        const values = line.split("\t");
+        return keys.reduce(function (data, key, i) {
+          data[key] = values[i].replace("\r", "");
+          return data;
+        }, {});
+      })
+      .filter((data) => data);
+    return dataList;
   }
 }
