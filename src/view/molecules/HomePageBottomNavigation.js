d7bb69f6d669c84f20a7ca46671984a7808f8846
nuuuwan <nuuuwan@gmail.com>
2022-10-28 10:38:01 +0530

    style: removed paper

--- src/view/molecules/HomePageBottomNavigation.js
+++ src/view/molecules/HomePageBottomNavigation.js
@@ -1,15 +1,9 @@
 import React from "react";
 
 import BottomNavigation from "@mui/material/BottomNavigation";
-import Paper from "@mui/material/Paper";
 
 export default function HomePageBottomNavigation() {
   return (
-    <Paper
-      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-      elevation={3}
-    >
-      <BottomNavigation></BottomNavigation>
-    </Paper>
+    <BottomNavigation />
   );
 }
