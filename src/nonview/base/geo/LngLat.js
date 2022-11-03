13ec158428fe5b01c0878820bcefacb60b2e9000
nuuuwan <nuuuwan@gmail.com>
2022-11-03 22:03:55 +0530

    cleanup: - fromGeoJSON

--- src/nonview/base/geo/LngLat.js
+++ src/nonview/base/geo/LngLat.js
@@ -34,19 +34,6 @@ export default class LngLat {
     }, []);
   }
 
-  static fromGeoJSON(geoJSON) {
-    return geoJSON.features.reduce(function (lngLatList, feature) {
-      const geometry = feature.geometry;
-      const geoType = geometry.type;
-      if (geoType === "MultiPolygon") {
-        return geometry.coordinates.reduce(function (lngLatList, polygonList) {
-          return [].concat(lngLatList, LngLat.fromPolygonList(polygonList));
-        }, lngLatList);
-      }
-      throw Error("Unknown geometry.type: " + geoType);
-    }, []);
-  }
-
   static MIN = new LngLat(-180, -180);
   static MAX = new LngLat(180, 180);
 }
