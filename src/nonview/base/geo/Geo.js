c4dcc27b33f4302da8fa2ebf4dce6370ebcc5cc5
nuuuwan <nuuuwan@gmail.com>
2022-11-03 17:59:21 +0530

    feat: + Geo

---
+++ src/nonview/base/geo/Geo.js
@@ -0,0 +1,18 @@
+import EntTypes from "../../../nonview/base/EntTypes";
+import WWW from "../../../nonview/base/WWW";
+
+const URL_BASE =
+  "https://raw.githubusercontent.com/nuuuwan/gig-data/master/geo";
+
+
+export default class Geo {
+  static getURLForRegionID(regionID) {
+    const regionType = EntTypes.getEntType(regionID);
+    return `${URL_BASE}/${regionType}/${regionID}.json`;
+  }
+
+  static async getPolygonList(regionID) {
+    const url = Geo.getURLForRegionID(regionID);
+    return await WWW.json(url);
+  }
+}
